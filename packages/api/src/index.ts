4cb5274cd70c2fde7b778fe54ac070df0088479b
Bismit Panda <adnaptimsib333@gmail.com>
2024-10-11 20:30:30 +0530

    chore: update deps

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,16 +1,20 @@
 import { configSchema } from "@repo/config/schema";
 import { readFileSync } from "fs";
-import createClient from "openapi-fetch";
+import createFetchClient from "openapi-fetch";
+import createQueryClient from "openapi-react-query";
+
 import * as toml from "smol-toml";
 
 import type { paths } from "./openapi.d";
 
 const config = configSchema.parse(
-  toml.parse(readFileSync("/data/config.toml", { encoding: "utf-8" })),
+    toml.parse(readFileSync("/data/config.toml", { encoding: "utf-8" }))
 );
 
-export const client = createClient<paths>({
-  baseUrl: `${config.services.api_server_host}:${config.services.api_server_port}`,
+export const fetchClient = createFetchClient<paths>({
+    baseUrl: `${config.services.api_server_host}:${config.services.api_server_port}`,
 });
+
+export const apiQueryClient = createQueryClient(fetchClient);
 
 export * from "./openapi.d";
